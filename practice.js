a564ae56a6e0ae44b6f142cb5b78fdb649d23554
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2022-06-05 15:19:24 +0100

    if statement practice

---
+++ practice.js
@@ -0,0 +1,29 @@
+// Check if the person is old enough to enter the nightclub (21)
+// Log a suitable message to the console in both cases
+
+// let age = 19
+
+// // if age is less than 21 -> "You cannot enter the nightclub"
+// // else -> "Welcome!"
+
+// if (age < 21) {
+//     console.log("You cannot the nightclub")
+// } else {
+//     console.log("Welcome!")
+// }
+
+// Check if the person is eligible for a birthday card from the King (100)
+
+let age = 100
+
+// if age is less than 100 -> "Not eligible yet"
+// else if 100 -> "Happy birthday! Here's your card from the King"
+// else -> "Not eligible, you've already received one"
+
+if (age < 100) {
+    console.log("Not eligible yet")
+} else if (age === 100) {
+    console.log("Happy birthday! Here's your card from the King")
+} else {
+    console.log("Not eligible, you've already received one")
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+let age = 19
+
+// // if age is less than 21 -> "You cannot enter the nightclub"
+// // else -> "Welcome!"
+
+// if (age < 21) {
+//     console.log("You cannot the nightclub")
+// } else {
+//     console.log("Welcome!")

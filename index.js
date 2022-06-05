7af77435e012b5a9d5d5431d6cec23b65893fd06
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2022-06-05 16:37:11 +0100

    displaying the different outcomes

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 let firstCard = 5
-let secondCard = 17
+let secondCard = 15
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("outcome-el")
 
-if (sum <= 20) {
-    message = "Do you want to draw or stick?"
-} else if (sum === 21) {
-    message = "Nice! You've got Blackjack!"
-    hasBlackJack = true
-} else {
-    message = "Unlucky! You're out the game"
-    isAlive = false
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw or stick?"
+    } else if (sum === 21) {
+        message = "Nice! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "Unlucky! You're out the game"
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
-
-console.log(message)
